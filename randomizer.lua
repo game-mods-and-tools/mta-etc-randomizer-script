89db0d6db4a077b0ea8b3300606f366de438e6c7
a <a>
2024-04-10 12:54:46 -0500

    add script

---
+++ randomizer.lua
@@ -0,0 +1,526 @@
+landstalker = 400
+bravura = 401
+buffalo = 402
+linerunner = 403
+perennial = 404
+sentinel = 405
+dumper = 406
+firetruck = 407
+trashmaster = 408
+stretch = 409
+manana = 410
+infernus = 411
+voodoo = 412
+pony = 413
+mule = 414
+cheetah = 415
+ambulance = 416
+leviathan = 417
+moonbeam = 418
+esperanto = 419
+taxi = 420
+washington = 421
+bobcat = 422
+mrwhoopee = 423
+bfinjection = 424
+hunter = 425
+premier = 426
+enforcer = 427
+securicar = 428
+banshee = 429
+predator = 430
+bus = 431
+rhino = 432
+barracks = 433
+hotknife = 434
+trailer1 = 435
+previon = 436
+coach = 437
+cabbie = 438
+stallion = 439
+rumpo = 440
+rcbandit = 441
+romero = 442
+packer = 443
+monster = 444
+admiral = 445
+squalo = 446
+seasparrow = 447
+pizzaboy = 448
+tram = 449
+trailer2 = 450
+turismo = 451
+speeder = 452
+reefer = 453
+tropic = 454
+flatbed = 455
+yankee = 456
+caddy = 457
+solair = 458
+berkleysrcvan = 459
+skimmer = 460
+pcj600 = 461
+faggio = 462
+freeway = 463
+rcbaron = 464
+rcraider = 465
+glendale = 466
+oceanic = 467
+sanchez = 468
+sparrow = 469
+patriot = 470
+quadbike = 471
+coastguard = 472
+dinghy = 473
+hermes = 474
+sabre = 475
+rustler = 476
+zr350 = 477
+walton = 478
+regina = 479
+comet = 480
+bmx = 481
+burrito = 482
+camper = 483
+marquis = 484
+baggage = 485
+dozer = 486
+maverick = 487
+newschopper = 488
+rancher = 489
+fbirancher = 490
+virgo = 491
+greenwood = 492
+jetmax = 493
+hotringracer = 494
+sandking = 495
+blistacompact = 496
+policemaverick = 497
+boxville = 498
+benson = 499
+mesa = 500
+rcgoblin = 501
+hotringracer2 = 502
+hotringracer3 = 503
+bloodringbanger =504
+rancherlure = 505
+supergt = 506
+elegant = 507
+journey = 508
+bike = 509
+mountainbike = 510
+beagle = 511
+cropduster = 512
+stuntplane = 513
+tanker = 514
+roadtrain = 515
+nebula = 516
+majestic = 517
+buccaneer = 518
+shamal = 519
+hydra = 520
+fcr900 = 521
+nrg500 = 522
+hpv1000 = 523
+cementtruck = 524
+towtruck = 525
+fortune = 526
+cadrona = 527
+fbitruck = 528
+willard = 529
+forklift = 530
+tractor = 531
+combineharvester = 532
+feltzer = 533
+remington = 534
+slamvan = 535
+blade = 536
+freight = 537
+brownstreak = 538
+vortex = 539
+vincent = 540
+bullet = 541
+clover = 542
+sadler = 543
+firetruckladder = 544
+hustler = 545
+intruder = 546
+primo = 547
+cargobob = 548
+tampa = 549
+sunrise = 550
+merit = 551
+utilityvan = 552
+nevada = 553
+yosemite = 554
+windsor = 555
+monster2 = 556
+monster3 = 557
+uranus = 558
+jester = 559
+sultan = 560
+stratum = 561
+elegy = 562
+raindance = 563
+rctiger = 564
+flash = 565
+tahoma = 566
+savanna = 567
+bandito = 568
+freightflatbed = 569
+flatbed = 570 --streaktraintrailer =
+kart = 571
+mower = 572
+dune = 573
+sweeper = 574
+broadway = 575
+tornado = 576
+at400 = 577
+dft30 = 578
+huntley = 579
+stafford = 580
+bf400 = 581
+newsvan = 582
+tug = 583
+trailertankercommando = 584
+emperor = 585
+wayfarer = 586
+euros = 587
+hotdog = 588
+club = 589
+boxfreight = 590
+trailer3 = 591
+andromada = 592
+dodo = 593
+rccam = 594
+launch = 595
+policels = 596
+policesf = 597
+policelv = 598
+policeranger = 599
+picador = 600
+swat = 601
+alpha = 602
+phoenix = 603
+glendaledamaged = 604
+sadlerdamaged = 605
+baggagetrailercovered = 606
+baggagetraileruncovered = 607
+trailerstairs = 608
+boxvillemission = 609
+farmtrailer = 610
+streetcleantrailer = 611
+
+
+
+
+
+
+local heights = {
+    [landstalker] = 2.22,
+    [bravura] = 1.55,
+    [buffalo] = 1.55,
+	[linerunner] = 3.22,
+    [sentinel] = 1.55,
+    [dumper] = 4.44,
+    [firetruck] = 2.11,
+    [trashmaster] = 2.77,
+    [stretch] = 1.55,
+    [manana] = 1.55,
+    [infernus] = 1.55,
+    [voodoo] = 1.22,
+    [pony] = 2.22,
+	[mule] = 2.55,
+	[cheetah] = 1.55,
+    [ambulance] = 2.11,
+    [leviathan] = 3.55,
+    [moonbeam] = 1.99,
+    [esperanto] = 1.11,
+	[taxi] = 1.73,
+    [washington] = 1.55,
+    [bobcat] = 1.88,
+    [mrwhoopee] = 2.22,
+    [bfinjection] = 1.99,
+    [hunter] = 1.55,
+    [premier] = 1.55,
+    [enforcer] = 2.11,
+    [securicar] = 2.33,
+    [banshee] = 1.33,
+    [predator] = 1.55,
+    [bus] = 2.11,
+    [rhino] = 2.77,
+	[barracks] = 3.33,
+	[hotknife] = 2.11,
+    [trailer1] = 3.22,
+    [previon] = 1.55,
+    [coach] = 2.11,
+    [cabbie] = 1.55,
+    [stallion] = 1.55,
+    [rumpo] = 2.33,
+    [rcbandit] = 0.44,
+    [romero] = 1.55,
+    [packer] = 3.77,
+    [monster] = 2.55,
+    [admiral] = 1.55,
+    [squalo] = 1.55,
+    [seasparrow] = 1.55,
+	[pizzaboy] = 1.44,
+    [tram] = 1.55,
+    [trailer2] = 3.22,
+    [turismo] = 1.88,
+    [speeder] = 1.55,
+    [reefer] = 2.22,
+    [tropic] = 1.55,
+	[flatbed] = 3.55,
+	[yankee] = 3.33,
+    [caddy] = 1.11,
+    [solair] = 1.44,
+    [berkleysrcvan] = 2.11,
+    [skimmer] = 5.77,
+    [pcj600] = 1.33,
+    [faggio] = 1.55,
+    [tram] = 1.55,
+    [freeway] = 1.55,
+    [rcbaron] = 0.44,
+    [rcraider] = 0.44,
+    [glendale] = 1.55,
+    [oceanic] = 1.55,
+    [sanchez] = 1.66,
+    [sparrow] = 1.55,
+    [patriot] = 2.66,
+    [quadbike] = 1.66,
+    [coastguard] = 1.55,
+    [dinghy] = 2.11,
+    [hermes] = 1.55,
+    [sabre] = 1.55,
+    [rustler] = 4.44,
+	[zr350] = 1.55,
+	[walton] = 3.25,
+    [regina] = 1.88,
+	[comet] = 1.55,
+    [bmx] = 0.99,
+    [burrito] = 2.11,
+    [camper] = 2.55,
+    [marquis] = 1.55,
+    [baggage] = 1.11,
+    [dozer] = 3.33,
+    [maverick] = 1.77,
+    [newschopper] = 1.77,
+    [rancher] = 2.55,
+    [fbirancher] = 2.55,
+    [virgo] = 1.77,
+    [greenwood] = 1.55,
+    [jetmax] = 1.55,
+    [hotringracer] = 1.22,
+    [sandking] = 3.44,
+    [blistacompact] = 1.55,
+    [policemaverick] = 1.77,
+	[boxville] = 2.88,
+	[benson] = 2.55,
+    [rcgoblin] = 0.44,
+    [hotringracer2] = 1.22,
+    [hotringracer3] = 1.22,
+    [bloodringbanger] = 1.22,
+    [rancherlure] = 2.11,
+	[supergt] = 1.55,
+    [elegant] = 1.55,
+    [journey] = 2.44,
+    [bike] = 0.99,
+    [mountainbike] = 0.99,
+    [beagle] = 2.88,
+    [cropduster] = 4.55,
+    [stuntplane] = 3.66,
+    [tanker] = 3.44,
+	[roadtrain] = 3.55,
+    [nebula] = 1.77,
+    [majestic] = 1.55,
+    [buccaneer] = 1.11,
+    [shamal] = 5.55,
+    [hydra] = 5.55,
+    [fcr900] = 1.33,
+    [nrg500] = 1.33,
+	[hpv1000] = 1.55,
+    [cementtruck] = 3.66,
+	[towtruck] = 2.77,
+    [fortune] = 1.33,
+    [cadrona] = 1.55,
+    [fbitruck] = 2.44,
+    [willard] = 1.55,
+    [forklift] = 1.55,
+    [tractor] = 2.33,
+    [combineharvester] = 3.77,
+    [feltzer] = 1.55,
+    [remington] = 1.11,
+    [slamvan] = 0.99,
+    [blade] = 1.11,
+    [freight] = 1.55,
+    [brownstreak] = 1.55,
+    [vortex] = 1.22,
+    [vincent] = 1.88,
+	[bullet] = 1.77,
+    [clover] = 1.55,
+    [sadler] = 1.99,
+    [firetruckladder] = 2.11,
+    [hustler] = 1.55,
+    [intruder] = 1.55,
+    [primo] = 1.55,
+    [cargobob] = 5.55,
+    [tampa] = 1.55,
+    [sunrise] = 1.55,
+    [merit] = 1.55,
+	[utilityvan] = 1.88,
+    [nevada] = 5.55,
+    [yosemite] = 1.99,
+    [windsor] = 1.55,
+    [monster2] = 2.55,
+    [monster3] = 2.55,
+	[uranus] = 1.38,
+	[jester] = 1.45,
+	[sultan] = 1.61,
+    [stratum] = 1.55,
+	[elegy] = 1.44,
+    [raindance] = 1.88,
+    [rctiger] = 0.44,
+	[flash] = 1.44,
+    [tahoma] = 1.55,
+    [savanna] = 1.22,
+    [bandito] = 1.55,
+    [freightflatbed] = 1.55,
+    [flatbed] = 3.11,
+--  [streaktraintrailer] = 3.55,
+    [kart] = 0.11,
+    [mower] = 1.11,
+	[dune] = 2.59,
+    [sweeper] = 1.22,
+    [broadway] = 0.89,
+    [tornado] = 0.99,
+    [at400] = 3.55,
+	[dft30] = 3.44,
+    [huntley] = 1.99,
+    [stafford] = 1.55,
+    [bf400] = 1.33,
+    [newsvan] = 1.99,
+    [tug] = 0.99,
+    [trailertankercommando] = 4.11,
+    [emperor] = 1.55,
+    [wayfarer] = 1.88,
+    [euros] = 1.55,
+    [hotdog] = 2.11,
+    [club] = 1.55,
+    [boxfreight] = 1.55,
+    [trailer3] = 3.22,
+    [andromada] = 4.55,
+    [dodo] = 2.88,
+    [rccam] = 0.44,
+    [launch] = 1.55,
+    [policels] = 1.55,
+    [policesf] = 1.55,
+    [policelv] = 1.55,
+    [policeranger] = 2.11,
+    [picador] = 1.88,
+    [swat] = 2.77,
+    [alpha] = 1.55,
+    [phoenix] = 1.55,
+    [glendaledamaged] = 1.55,
+    [sadlerdamaged] = 1.99,
+    [baggagetrailercovered] = 2.22,
+    [baggagetraileruncovered] = 2.22,
+    [trailerstairs] = 3.11,
+	[boxvillemission] = 2.88,
+    [farmtrailer] = 1.33,
+    [streetcleantrailer] = 2.11,
+}
+
+local availablevehicles = {
+    -- 4-door sedans
+    admiral,glendaledamaged,elegant,emperor,glendale,greenwood,intruder,merit,nebula,oceanic,premier,primo,sentinel,stafford,sunrise,tahoma,vincent,washington,willard,
+    policels,policesf,policelv,
+    -- coupes,
+    alpha,blistacompact,bravura,buccaneer,cadrona,club,esperanto,euros,feltzer,fortune,hermes,hustler,majestic,manana,picador,previon,virgo,
+    -- muscle
+    buffalo,clover,phoenix,sabre,stallion,tampa,
+    hotknife,
+    -- suvs
+    huntley,landstalker,rancher,rancherlure,mesa,sandking,
+    patriot,fbirancher,fbitruck,policeranger,
+    -- stationwagons
+    perennial,regina,romero,solair,stratum,flash,
+    -- lowrider
+    blade,broadway,remington,savanna,slamvan,tornado,voodoo,
+    -- sports/racer
+    banshee,bullet,cheetah,comet,elegy,hotringracer,hotringracer2,hotringracer3,infernus,jester,sultan,supergt,turismo,uranus,windsor,zr350,
+    -- motorcycles
+    bf400,faggio,fcr900,freeway,nrg500,pcj600,pizzaboy,sanchez,wayfarer,hpv1000,
+    -- bicycles
+    bike,bmx,mountainbike,
+    -- taxis
+    taxi,cabbie,
+    -- buses
+    bus,coach,
+    -- monsters
+    monster,monster2,monster3,
+    -- pickups
+    towtruck,utilityvan,bobcat,sadler,sadlerdamaged,walton,yosemite,
+    -- small vans
+    hotdog,mrwhoopee,berkleysrcvan,burrito,moonbeam,newsvan,pony,rumpo,
+    -- large vans
+    securicar,benson,boxville,boxvillemission,mule,yankee,journey,
+    ambulance,
+    -- trucks
+    dft30,dune,flatbed,linerunner,packer,roadtrain,tanker,
+    cementtruck,
+    firetruck,firetruckladder,
+    barracks,enforcer,
+    -- large industrial
+    combineharvester,dozer,dumper,
+    -- various small vehicles
+    baggage,sweeper,tractor,mower,tug,caddy,
+    -- trailers (driveable)
+    baggagetraileruncovered,baggagetrailercovered,streetcleantrailer,
+    -- trailers (undriveable)
+--    farmtrailer,trailer,trailertankercommando,trailerstairs,trailer1,trailer2,trailer3,
+    -- airplanes
+--    andromada,at400,beagle,cropduster,dodo,hydra,nevada,rustler,shamal,skimmer,stuntplane,
+--    rcbaron,
+    -- helicopters
+  --  cargobob,hunter,leviathan,maverick,newschopper,policemaverick,raindance,seasparrow,sparrow,
+  --  rcgoblin,rcraider,
+    -- boats
+ --   coastguard,dinghy,jetmax,launch,marquis,predator,reefer,speeder,squalo,tropic,
+    -- trains
+ --   boxfreight,brownstreak,freight,freightflatbed,tram,
+    -- RC cars
+ --   rcbandit,rccam,rctiger,
+    -- other
+    trashmaster,
+    forklift,
+    kart,
+    rhino,
+    bandito,
+    bfinjection,
+    quadbike
+} -- don't forget about the trailing comma...
+
+for k in pairs(heights) do
+	availablevehicles[#availablevehicles + 1] = k
+end
+
+function vehicleRandomiser(checkpoint)
+  local veh = getPedOccupiedVehicle(source)
+  local vehmodel = getElementModel(veh)
+  local newveh = availablevehicles[math.random(#availablevehicles)]
+
+  local oldHeight = heights[vehmodel]
+
+  if 1 <= checkpoint then
+    setElementModel(veh, newveh)
+  end
+
+  local newHeight = heights[newveh]
+
+  local x, y, z = getElementPosition(veh)
+  setElementPosition(veh, x, y, z + newHeight / 2 - oldHeight / 2 + 0.05)
+end
+
+addEvent('onPlayerReachCheckpoint')
+addEventHandler('onPlayerReachCheckpoint', getRootElement(), vehicleRandomiser)
